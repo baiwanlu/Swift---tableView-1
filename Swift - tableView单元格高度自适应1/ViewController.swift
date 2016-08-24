cbfc3a3c6c8882042d0424c0b571a88e6189d28e
baiwanlu <870533813@qq.com>
2016-08-24 10:49:03 +0800

    tableview
    文字的自适应

---
+++ Swift - tableView单元格高度自适应1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift - tableView单元格高度自适应1
+//
+//  Created by 道标朱 on 16/8/24.
+//  Copyright © 2016年 道标朱. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
